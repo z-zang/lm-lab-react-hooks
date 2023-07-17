b35385d1f430daf5870bfdadb0c463ac57ff4299
Zichao Zang <zang.code@gmail.com>
2023-07-17 10:42:16 +0100

    implement useEffect (wip)

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,13 +1,44 @@
 // make an api call using useEffect
 
-export const APICall = () => {
+import { useEffect, useState } from "react"
 
-    // Your code here!
+interface TTodo {
+    completed: boolean,
+    title: string,
+    id: number,
+    userId: number
+}
+
+export const APICall = () => {
+    const [url, setUrl] = useState('https://jsonplaceholder.typicode.com/todos/1')
+    const [todo, setTodo] = useState<TTodo | null>(null)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await fetch(url)
+                const data = await res.json()
+                setTodo(data)
+            } catch (err) {
+                console.log(err)
+            }
+        };
+
+        fetchData();
+        return () => {
+            // cleanup
+        }
+    }, [url]);
 
     return (
         <>
             <h2>useEffect</h2>
-            <p>Your todo here!</p>
+            {todo && (
+                <div>
+                    <h2>{todo.title}</h2>
+                    <p>Completed: {todo.completed.toString()}</p>
+                </div>
+            )}
         </>
     )
 }
